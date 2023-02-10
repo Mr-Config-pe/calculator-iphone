3c66ebccdf0349b5d1cb00b3e4f30c9426f9eb01
mr-config-pe <mr.configperu@gmail.com>
2023-02-10 16:58:07 -0500

    Agregando algunos botones extra a calculadora y CSS de iPhone

---
+++ src/components/Calculator.js
@@ -0,0 +1,128 @@
+import React, { useState } from "react";
+import "../assets/css/Calculator.css";
+import backcel from "../assets/images/back-calculator.png";
+
+function Calculator() {
+  const [display, setDisplay] = useState("0");
+  const [doubleClick, setDoubleClick] = useState(false);
+
+  const handleClick = (value) => {
+    if (display.length < 50) {
+      setDisplay(display === "0" ? value : display + value);
+    }
+  };
+
+  const handleEvaluate = () => {
+    setDisplay(eval(display).toString());
+  };
+
+  const handleClear = () => {
+    setDisplay("0");
+  };
+
+  const handleBackspace = () => {
+    setDisplay(display.length > 1 ? display.slice(0, -1) : "0");
+  };
+
+  const handleDecimal = () => {
+    setDisplay(display.includes(".") ? display : display + ".");
+  };
+
+  const handlePercentage = () => {
+    setDisplay((eval(display) / 100).toString());
+  };
+
+  const handleClearOrBackspace = () => {
+    if (doubleClick) {
+      handleClear();
+      setDoubleClick(false);
+    } else {
+      handleBackspace();
+      setDoubleClick(true);
+    }
+    setTimeout(() => {
+      setDoubleClick(false);
+    }, 500);
+  };
+
+  const handleSignChange = () => {
+    setDisplay((parseFloat(display) * -1).toString());
+  };
+
+  return (
+    // eslint-disable-next-line
+    <div className="calculator">
+      <img src={backcel} className="back-cel" />
+      <div className="display">{display}</div>
+      <div className="row">
+        <button onClick={handleClearOrBackspace} className="btn-gris">
+          {doubleClick ? "AC" : "C"}
+        </button>
+        <button onClick={handleSignChange} className="btn-gris">
+          +/-
+        </button>
+        <button onClick={handlePercentage} className="btn-gris">
+          %
+        </button>
+        <button onClick={() => handleClick("/")} className="symbol">
+          ÷
+        </button>
+      </div>
+      <div className="row">
+        <button onClick={() => handleClick("7")} className="number-calc">
+          7
+        </button>
+        <button onClick={() => handleClick("8")} className="number-calc">
+          8
+        </button>
+        <button onClick={() => handleClick("9")} className="number-calc">
+          9
+        </button>
+        <button onClick={() => handleClick("*")} className="symbol">
+          ×
+        </button>
+      </div>
+      <div className="row">
+        <button onClick={() => handleClick("4")} className="number-calc">
+          4
+        </button>
+        <button onClick={() => handleClick("5")} className="number-calc">
+          5
+        </button>
+        <button onClick={() => handleClick("6")} className="number-calc">
+          6
+        </button>
+        <button onClick={() => handleClick("-")} className="symbol">
+          -
+        </button>
+      </div>
+      <div className="row">
+        <button onClick={() => handleClick("1")} className="number-calc">
+          1
+        </button>
+        <button onClick={() => handleClick("2")} className="number-calc">
+          2
+        </button>
+        <button onClick={() => handleClick("3")} className="number-calc">
+          3
+        </button>
+        <button onClick={() => handleClick("+")} className="symbol">
+          +
+        </button>
+      </div>
+      <div className="row">
+        <button onClick={() => handleClick("0")} className="number-calc cero">
+          0
+        </button>
+        <button onClick={handleDecimal} className="symbol">
+          .
+        </button>
+        <button onClick={handleEvaluate} className="symbol">
+          =
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default Calculator;
